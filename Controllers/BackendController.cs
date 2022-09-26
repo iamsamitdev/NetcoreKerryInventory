e786e9d4a7bcd7e836e97dc1dee3c94878dc8cab
Samit Koyom <samitkoyom@gmail.com>
2022-09-27 01:40:57 +0700

    first commit

---
+++ Controllers/BackendController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetcoreKerryInventory.Controllers;
+public class BackendController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Product()
+    {
+        return View();
+    }
+
+}
---
+++ Controllers/FrontendController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreInventory.Controllers;
+
+public class FrontendController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult About()
+    {
+        return View();
+    }
+
+    public ActionResult Webdev()
+    {
+        return View();
+    }
+
+    public ActionResult Mobiledev()
+    {
+        return View();
+    }
+
+    public ActionResult Graphic()
+    {
+        return View();
+    }
+
+    // เรียกแสดงแบบฟอร์มลงทะเบียน
+    [HttpGet]
+    public ActionResult Register()
+    {
+        return View();
+    }
+
+    // เรียกแสดงแบบฟอร์มลงเข้าสุ่ระบบ
+    [HttpGet]
+    public ActionResult Login()
+    {
+        return View();
+    }
+
+}
+
